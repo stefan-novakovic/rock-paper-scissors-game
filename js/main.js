0e889861db39adfc2e4135d5d706e9e25faa179c
Stefan Novakovic <snovakovic3@gmail.com>
2023-11-07 01:40:07 +0100

    Second commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 const initApp = () => {
   console.log("DOMContentLoaded: Completed!");
+  playerChoice();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const playerChoice = () => {
+  const p1Images = document.querySelectorAll(
+    ".gameboard__player .icon_wrapper img"
+  );
+  p1Images.forEach((img) => {
+    img.addEventListener("click", (event) => {
+      p1Images.forEach((img) => {
+        if (event.target === img) {
+          img.parentElement.classList.add("selected");
+        } else {
+          img.parentElement.classList.add("not-selected");
+        }
+      });
+    });
+  });
+};

79be97c636b49d8845822871b30899b81cd3fa15
Stefan Novakovic <snovakovic3@gmail.com>
2023-11-07 03:57:42 +0100

    Fourth commit (added player updated message)

--- js/main.js
+++ js/main.js
@@ -1,23 +1,34 @@
 const initApp = () => {
   console.log("DOMContentLoaded: Completed!");
-  playerChoice();
+  startGame();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
+
+const startGame = () => {
+  playerChoice();
+};
 
 const playerChoice = () => {
   const p1Images = document.querySelectorAll(
     ".gameboard__player .icon_wrapper img"
   );
+
   p1Images.forEach((img) => {
     img.addEventListener("click", (event) => {
       p1Images.forEach((img) => {
         if (event.target === img) {
           img.parentElement.classList.add("selected");
+          updatePlayerMessage(event.target.alt);
         } else {
           img.parentElement.classList.add("not-selected");
         }
       });
     });
   });
+};
+
+const updatePlayerMessage = (playerChoice) => {
+  const p1Message = document.querySelector(".gameboard__player h3");
+  p1Message.textContent += " " + playerChoice;
 };

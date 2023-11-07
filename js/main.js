a55e74584e1c80cea0bc383714d0bf1ea21a2cbd
Stefan Novakovic <snovakovic3@gmail.com>
2023-11-07 05:48:17 +0100

    Fourth commit (animations finished)

--- js/main.js
+++ js/main.js
@@ -15,20 +15,68 @@ const playerChoice = () => {
   );
 
   p1Images.forEach((img) => {
-    img.addEventListener("click", (event) => {
-      p1Images.forEach((img) => {
-        if (event.target === img) {
-          img.parentElement.classList.add("selected");
-          updatePlayerMessage(event.target.alt);
-        } else {
-          img.parentElement.classList.add("not-selected");
-        }
-      });
-    });
+    img.addEventListener(
+      "click",
+      (event) => {
+        p1Images.forEach((img) => {
+          if (event.target === img) {
+            img.parentElement.classList.add("selected");
+            img.parentElement.classList.add("no-hover");
+            updatePlayerMessage(event.target.alt.split(" ")[1]);
+            computerChoice();
+          } else {
+            img.setAttribute("tabindex", "-1");
+            img.parentElement.classList.add("not-selected");
+          }
+        });
+      },
+      { once: true }
+    );
   });
 };
 
 const updatePlayerMessage = (playerChoice) => {
-  const p1Message = document.querySelector(".gameboard__player h3");
+  const p1Message = document.querySelector(".gameboard__player h2");
   p1Message.textContent += " " + playerChoice;
+};
+
+const computerChoice = () => {
+  const cpChoiceArray = ["Rock", "Paper", "Scissors"];
+  const cpChoiceIndex = Math.floor(Math.random() * 3);
+  const cpChoice = cpChoiceArray[cpChoiceIndex];
+  console.log(cpChoice);
+
+  const cpImages = document.querySelectorAll(
+    ".gameboard__computer .icon_wrapper img"
+  );
+
+  cpImages.forEach((img) => {
+    img.parentElement.classList.add("cp_images_fadeout_animation");
+  });
+  setTimeout(() => {
+    computerChoiceShow(cpChoice);
+  }, 4000);
+};
+
+const computerChoiceShow = (cpChoice) => {
+  const cpImages = document.querySelectorAll(
+    ".gameboard__computer .icon_wrapper img"
+  );
+
+  cpImages.forEach((img) => {
+    if (img.alt === cpChoice) {
+      img.parentElement.classList.remove("cp_images_fadeout_animation");
+      img.parentElement.classList.add("cp_images_fadein_animation");
+      setTimeout(() => {
+        updateComputerMessage(cpChoice);
+      }, 50);
+    } else {
+      img.parentElement.style.display = "none";
+    }
+  });
+};
+
+const updateComputerMessage = (cpChoice) => {
+  const cpMessage = document.querySelector(".gameboard__computer h2");
+  cpMessage.textContent += " " + cpChoice;
 };
